44a364e9dddf15216c5518b06934066ee0013cec
howlie190 <howlie190@gmail.com>
2022-07-18 08:53:53 +0800

    no message

--- NodeDepths/Solution1.cpp
+++ NodeDepths/Solution1.cpp
@@ -2,24 +2,27 @@
 
 using namespace std;
 
-class BinaryTree {
-public:
-  int value;
-  BinaryTree *left;
-  BinaryTree *right;
-
-  BinaryTree(int value) {
-    this->value = value;
-    left = nullptr;
-    right = nullptr;
-  }
-};
-int DepthSum(BinaryTree* tree, int sum)
+class BinaryTree
 {
-  if(tree != NULL)
-    return DepthSum(tree->left, sum + 1) + DepthSum(tree->right, sum + 1) + sum;
-  return 0;
+public:
+    int value;
+    BinaryTree *left;
+    BinaryTree *right;
+
+    BinaryTree(int value)
+    {
+        this->value = value;
+        left = nullptr;
+        right = nullptr;
+    }
+};
+int DepthSum(BinaryTree *tree, int sum)
+{
+    if (tree != NULL)
+        return DepthSum(tree->left, sum + 1) + DepthSum(tree->right, sum + 1) + sum;
+    return 0;
 }
-int nodeDepths(BinaryTree *root) {
-  return DepthSum(root, 0);
+int nodeDepths(BinaryTree *root)
+{
+    return DepthSum(root, 0);
 }
